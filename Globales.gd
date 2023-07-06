5b4b40581ea6747eb37e919f26e7ff6e08330516
VicoJulian <135705750+VicoJulian@users.noreply.github.com>
2023-07-06 07:55:41 -0300

    Add files via upload

---
+++ Globales.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+
+var Cofre_uno=false
+var Cofre_dos=false
+var Cofre_tres=false
+
+var Traje_uno=false
+var Traje_dos=false
+var Traje_tres=false
+
+var Llave_uno=false
+var Llave_dos=false
+
+var Puerta_uno_abierta=false
+var Puerta_dos_abierta=false
+
+var Vara_poder_verde=false
+
+#		HABILITO COFRE
+#		APARECE LLAVE 
+#		AGARRO LA LLAVE
+#		HABILITO PUERTA
+#
+#
+#
+#
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
